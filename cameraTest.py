d81687acecbf6d2cd22496268f960c6e69c3a8cf
Nolan <stevenson.r.nolan@gmail.com>
2024-02-07 11:41:59 -0700

    Nano First Push

---
+++ cameraTest.py
@@ -0,0 +1,104 @@
+# Import libraries
+#import numpy as np
+#import matplotlib as mpl
+#import pandas as pd
+import cv2
+
+# ArUco dictionaries
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+# Highlight the detected markers
+def aruco_display(corners, ids, rejected, image):
+    if(len(corners) > 0): # Are any aruco tags detected
+        
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4,2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners  # Get the corners
+
+            # Cast the data to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            # Draw the lines for the AR tag detection
+            cv2.line(image, topLeft, topRight, (0, 0, 255), 2)
+            cv2.line(image, topRight, bottomRight, (0, 0, 255), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 0, 255), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 0, 255), 2)
+
+    return image
+
+# Get predefined dictionary
+aruco_type = "DICT_6X6_250"
+testDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_type])
+
+# %% Video Capture With Webcam
+#cap = cv2.VideoCapture(0)
+
+# Open the video capture object for the specified camera index
+#cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+# Set the camera index to 0
+camera_index = 0
+
+# Define the GStreamer pipeline string
+pipeline = f'nvarguscamerasrc sensor-id={camera_index} ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+pipeline = f'nvarguscamerasrc sensor-id={camera_index} ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+
+# Create a VideoCapture object with the GStreamer pipeline
+cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+
+
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+while cap.isOpened():
+    # Get the current video feed frame
+    ret, img = cap.read()
+    
+    # Locate the Aruco tag
+    corners, ids, rejected = cv2.aruco.detectMarkers(img, testDict)
+    image = aruco_display(corners, ids, rejected, img)
+    
+	# Display the frame
+    cv2.imshow('frame', image)
+    
+	# Quit
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+    
+	
+cv2.destroyAllWindows()
+cap.release()	
+	
