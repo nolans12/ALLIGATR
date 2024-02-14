44e429d64eeceb731c333c9107e58cc8b55a99b1
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-02-14 11:03:21 -0700

    Update

--- HUB/ros_packages/gatr_computer_vision/src/ARtag_node.py
+++ HUB/ros_packages/gatr_computer_vision/src/ARtag_node.py
@@ -33,25 +33,26 @@ if __name__ == '__main__': # <- Executable
     faux_camera = False
     attempts = 0
 
-    # Try to open the 0 index for the webcam
-    camera_index = 0    # Webcam
+    # Try to open the 0 index for the primary camera
+    camera_index = 0   
 
     while not camera_found:
-        # Implement code to check for camera connection here
+        # Setup the GStreamer Pipeline
+        pipeline = f'nvarguscamerasrc sensor-id={camera_index} ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
 
-        # Create a VideoCapture object using the webcam
-        cap = cv2.VideoCapture(camera_index)
+        # Create a VideoCapture object with the GStreamer pipeline
+        cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
         # Check if the camera opened successfully
         if cap.isOpened():
             camera_found = True     # Camera is found
-            rospy.loginfo("Camera Connected")
+            rospy.loginfo("Camera " + str(camera_index) + " Connected!")
             break            
 
         rospy.sleep(3.0) # Sleep for 1 second
 
         # If camera is not found, output an error message
-        rospy.logwarn("Camera not found. Trying next camera index")
+        rospy.logwarn("Camera not found. Trying again...")
         camera_index += 1
 
         if camera_index > 10:
@@ -70,7 +71,7 @@ if __name__ == '__main__': # <- Executable
             # Search for Aruco tag
             corners, ids, rejected = cv2.aruco.detectMarkers(img, finalDict)
 
-            # Output the detected corners
+            # Output the detected corners if detected
             if corners:
                 out_str = "AR Tag Detected %s" % rospy.get_time()
             else:
@@ -84,5 +85,6 @@ if __name__ == '__main__': # <- Executable
         pub.publish(out_str)
         rate.sleep()
 
-
+    cv2.destroyAllWindows()         # Close everything and release the camera
+    cap.release()
     rospy.loginfo("End of program") # This will output to the terminal
