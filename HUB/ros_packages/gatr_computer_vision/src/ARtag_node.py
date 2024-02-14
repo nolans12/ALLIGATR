a8f88961ca903008e911a0fa74a196d901085d44
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-02-14 13:31:23 -0700

    Update ARtag_node.py

--- HUB/ros_packages/gatr_computer_vision/src/ARtag_node.py
+++ HUB/ros_packages/gatr_computer_vision/src/ARtag_node.py
@@ -45,12 +45,12 @@ if __name__ == '__main__': # <- Executable
     attempts = 0
 
     # Try to open the 0 index for the secondary camera
-    camera_index = 0   
+    camera_index = 1   
 
     while not camera_found:
         # Setup the GStreamer Pipeline
         #pipeline = f'nvarguscamerasrc sensor-id={camera_index} ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
-        pipeline = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+        pipeline = 'nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
 
 
         # Create a VideoCapture object with the GStreamer pipeline
