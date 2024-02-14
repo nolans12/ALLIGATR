15afaf94ee3aab69b85a5663ce473df17948946c
ckohl10 <carson.kohlbrenner@gmail.com>
2024-02-13 22:52:35 -0700

    Updated gazebo_engage

--- HUB/README.md
+++ HUB/README.md
@@ -1,10 +1,17 @@
 # Operating the HUB
 
-**Building the HUB**: The HUB needs to have a 'catkin_ws' compiled in the home directory for ROS to work, a main component of the HUB.
+**Building the HUB**: The HUB needs to have a 'catkin_ws' compiled in the home directory for ROS to work, a main component of the HUB. To automatically build this, run `bash create_ws.sh` while in the HUB directory. If an error is encountered, you can create it without MAVROS by running `bash create_ws.sh bare`. This will likely lead to errors later on however unless the workspace has been configured followign these steps: https://github.com/Intelligent-Quads/iq_tutorials/blob/master/docs/installing_ros.md. 
+
+**Engaging the Drone**: From the HUB directory run `bash engage.sh main.cpp` to begin the autonomous procedure laid out in the main.cpp script. To run a simulation of the mission, run `bash gazebo_engage.sh main.cpp`.
 
 # Authors:
+
 **TEAM ALLIGATR - University of Colorado Boulder AES Senior Projects 2024**
+
 **Carson Kohlbrenner**: Interfacing and compiling
+
 **Thomas Dunnington**: Computer vision
+
 **Zane Vandivere**: Mission planning
+
 **Nolan Stevenson**: Simulation and mission planning
--- HUB/gazebo_engage.sh
+++ HUB/gazebo_engage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Sets the font to be bigger on Xterm
+xrdb -merge ~/.Xresources
+
 # Save the file name as a variable called SOURCE_FILE
 SOURCE_FILE=$1
 
@@ -10,11 +13,11 @@ function cleanup {
 trap cleanup EXIT
 
 # Create a new catkin_ws if one doesnt exist
-if [ -d "~/catkin_ws" ]; then
-    echo "Catkin Workspace Detected!"
-else
-    bash src/build_catkin_ws.sh
-fi
+# if [ -d "~/catkin_ws" ]; then
+#     echo "Catkin Workspace Detected!"
+# else
+#     bash src/build_catkin_ws.sh
+# fi
 
 
 # This will clear any previous builds before building again
@@ -24,7 +27,11 @@ mkdir -p build
 #Compile the ROS packages
 CURRENT_DIR=$(pwd) #Save the current directory as a variable
 cd ~/catkin_ws
-catkin_make
+
+# Use catkin_make if not using MAVROS and catkin build if using MAVROS
+# catkin_make
+catkin build
+
 source devel/setup.bash
 cd ${CURRENT_DIR} #Go back to the build directory
 
@@ -36,20 +43,24 @@ run_node()
     # Make sure the node being ran is an executable
     cd ${CURRENT_DIR}/ros_packages/${PKG_NAME}
     chmod +x scripts/${NODE_NAME}
+    chmod +x src/${NODE_NAME}
     cd ${CURRENT_DIR}
 
     # Launch the XTERM terminal and run the ROS node
     # Make copies of this line of code for any additional nodes
     xterm -e "source ~/.bashrc; rosrun $PKG_NAME $NODE_NAME; exec bash" &
+    sleep 1
 }
 
 #Opens a roscore terminal. If one already exists, it will close itself
 xterm -e "source ~/.bashrc; roscore; exit; exec bash" &
+sleep 2
 
 
 ################### ADD PROCESSES HERE ######################
 
-run_node ros_beginner_package first_cpp_node.cpp
+run_node gatr_computer_vision blob_detection_node.py
+run_node gatr_computer_vision ARtag_node.py
 
 #############################################################
 
