fd737c1b77f7bcf3cb73ee379285a6a6edc33f7c
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-02-19 13:33:58 -0700

    Update engage.sh

--- HUB/engage.sh
+++ HUB/engage.sh
@@ -91,10 +91,10 @@ run_node gatr_computer_vision ARtag_node.py AR_Tag_Detection_Node
 run_node gatr_computer_vision localize_node.py Localize_Node
 
 # Mision Planner Node
-run_node gatr_missionplanner mp_node Mission_Planner_Node
+#run_node gatr_missionplanner mp_node Mission_Planner_Node
 
 # Start the MAVLINK connection to cube, opening on ttyTHS1 port
-xterm -T "mavlink" -e "sudo mavproxy.py --master=/dev/ttyTHS1" &  
+#xterm -T "mavlink" -e "sudo mavproxy.py --master=/dev/ttyTHS1" &  
 
 #Start the MAVROS node
 #xterm -T "mavros" -e "roslaunch mavros apm.launch fcu_url:=/dev/ttyTHS1:57600@" &
