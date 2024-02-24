023846120345f48416eb3a6132e4e9e41913a411
Nolan <stevenson.r.nolan@gmail.com>
2024-02-24 16:10:54 -0700

    Autofocus Images

--- CV/Calibration/dataCollection.py
+++ CV/Calibration/dataCollection.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Open the camera
-camera_index = 0
+camera_index = 1
 pipeline = 'nvarguscamerasrc sensor-id=' + str(camera_index) + ' ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
 cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
@@ -16,7 +16,7 @@ while cap.isOpened():
     if k == ord('q'):
         break
     elif k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite('WideImages/img' + str(num) + '.png', img)
+        cv2.imwrite('AutoImages/img' + str(num) + '.png', img)
         print("Image Saved")
         num += 1
 
