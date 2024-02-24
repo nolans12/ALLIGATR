d3e64103b255fb2f26b7c6f0a710cc87710c7f1b
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-02-24 15:48:31 -0700

    Update

--- CV/Calibration/dataCollection.py
+++ CV/Calibration/dataCollection.py
@@ -1,6 +1,9 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+# Open the camera
+camera_index = 0
+pipeline = 'nvarguscamerasrc sensor-id=' + str(camera_index) + ' ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
 num = 0
 
@@ -13,13 +16,12 @@ while cap.isOpened():
     if k == ord('q'):
         break
     elif k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite('Calibration/Images/img' + str(num) + '.png', img)
-        print("image saved!")
+        cv2.imwrite('Images/img' + str(num) + '.png', img)
+        print("Image Saved")
         num += 1
 
     cv2.imshow('Img',img)
 
 # Release and destroy all windows before termination
 cap.release()
-
 cv2.destroyAllWindows()
