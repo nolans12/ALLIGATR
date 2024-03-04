4ccd5318f035cc055e80ae42d7ec0cbdbacdebe0
Zane Vandivere <zvandivere@gmail.com>
2024-03-04 12:33:24 -0700

    closest RGV func updated

--- HUB/ros_packages/gatr_missionplanner/src/helper_funcs.cpp
+++ HUB/ros_packages/gatr_missionplanner/src/helper_funcs.cpp
@@ -1,5 +1,4 @@
 #include "headers/helper_funcs.h"
-#include <math.h>
 
 // Returns the indices of the smallest value in a 2x2 array
 short int* minInd(float arr[2][2]) {
@@ -22,10 +21,10 @@ int sign(float num) {
     return (num >= 0) - (num < 0);
 }
 
-bool isRGVAClosest() {
+bool isRGVAClosest(uas drone, environment env) {
     double distA, distB;
-    distA = pow(pow(drone.state[0]-env.rgvAPostion[0], 2) + pow(drone.state[1]-env.rgvAPosition[1], 2), 0.5);
-    distB = pow(pow(drone.state[0]-env.rgvBPostion[0], 2) + pow(drone.state[1]-env.rgvBPosition[1], 2), 0.5);
+    distA = pow(pow(drone.state[0]-env.rgvAPosition[0], 2) + pow(drone.state[1]-env.rgvAPosition[1], 2), 0.5);
+    distB = pow(pow(drone.state[0]-env.rgvBPosition[0], 2) + pow(drone.state[1]-env.rgvBPosition[1], 2), 0.5);
 
     return (distA <= distB);
 }
