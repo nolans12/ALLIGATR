fdc0708ef1334b849076d15fd935f2cfa6c0fe78
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-03-12 14:12:08 -0600

    Update

--- CV/ARTest.py
+++ CV/ARTest.py
@@ -80,17 +80,17 @@ while cap.isOpened():
         ret, img = cap.read()
 
         # Locate the Aruco tag
-        corners, ids, rejected = cv2.aruco.detectMarkers(img, finalDict)
-        image = aruco_display(corners, ids, rejected, img)
+        #corners, ids, rejected = cv2.aruco.detectMarkers(img, finalDict)
+        #image = aruco_display(corners, ids, rejected, img)
 
         # Update frames
-        frames.append(image)
+        #frames.append(image)
 
         # Save the frame
         writeObj.write(img)
         
         # Display the frame
-        cv2.imshow('frame', image)
+        #cv2.imshow('frame', image)
         
         # Quit
         key = cv2.waitKey(1) & 0xFF
