0b23b3946fd3117d25ce4dafe73016807706e404
ckohl10 <carson.kohlbrenner@gmail.com>
2024-03-16 13:03:56 -0600

    Working Mission Planner up to joint phase

--- HUB/ros_packages/gatr_missionplanner/src/MissonPlanner.cpp
+++ HUB/ros_packages/gatr_missionplanner/src/MissonPlanner.cpp
@@ -301,7 +301,7 @@ void MissionPlanner::fine_phase(){
 
         else{
             // Output how long it has been localizing for
-            ROS_INFO("RGV A has been finely localized for %f seconds...", (ros::Time::now() - fine_time_engaged).toSec());
+            ROS_INFO("RGV A has been finely localized for %f seconds...", fine_length.toSec());
         }
     }
 
@@ -330,7 +330,7 @@ void MissionPlanner::fine_phase(){
 
         else{
             // Output how long it has been localizing for
-            ROS_INFO("RGV B has been finely localized for %f seconds...", (ros::Time::now() - fine_time_engaged).toSec());
+            ROS_INFO("RGV B has been finely localized for %f seconds...", fine_length.toSec());
         }
     }
 
@@ -674,56 +674,56 @@ std::vector<double> MissionPlanner::joint_motion(std::vector<double> waypoint) {
     return waypoint;
 }
 
-std::vector<double> MissionPlanner::coarse(std::vector<double> waypoint) {
-    /* Circles around an RGV
-     * Output:
-     *         waypoint - 1x3 double vector of commanded point
-     * Makes use of the uas theta iterative property to track where in the orbit
-     * the drone is. Makes additional adjustments to the commanded point to
-     * avoid exiting the boundary, when necessary. 
-     */
-    std::vector<double> target;
-    double x, y;
-    double thetaStep = 6 * M_PI/180; // change in circle angle over time step [rad]
-    int r = 10; // radius of the orbital path [m]
+// std::vector<double> MissionPlanner::coarse(std::vector<double> waypoint) {
+//     /* Circles around an RGV
+//      * Output:
+//      *         waypoint - 1x3 double vector of commanded point
+//      * Makes use of the uas theta iterative property to track where in the orbit
+//      * the drone is. Makes additional adjustments to the commanded point to
+//      * avoid exiting the boundary, when necessary. 
+//      */
+//     std::vector<double> target;
+//     double x, y;
+//     double thetaStep = 6 * M_PI/180; // change in circle angle over time step [rad]
+//     int r = 10; // radius of the orbital path [m]
 
-    ////// TODO: add branches determining which RGV to target //////
-    // if both RGVs in view, target closest one
-    // if only one RGV in view, target that one
-    // if no RGV in view, target most recent RGV estimate (moving has put RGV out of camera view)
-    target = waypoint;
+//     ////// TODO: add branches determining which RGV to target //////
+//     // if both RGVs in view, target closest one
+//     // if only one RGV in view, target that one
+//     // if no RGV in view, target most recent RGV estimate (moving has put RGV out of camera view)
+//     target = waypoint;
 
-    // now get the commanded x,y
+//     // now get the commanded x,y
 
-    if (drone.theta == -1) {
-        // if coarse hasn't started yet, initialize the starting angle based
-        // on current drone position
-        drone.theta = atan2(drone.state[1]-target[1], drone.state[0]-target[0]);
-    }
-    // increment the angle of the orbit by thetaStep
-    drone.theta += thetaStep;
-    // set the x and y of the commanded point
-    x = target[0] + r*cos(drone.theta);
-    y = target[1] + r*sin(drone.theta);
+//     if (drone.theta == -1) {
+//         // if coarse hasn't started yet, initialize the starting angle based
+//         // on current drone position
+//         drone.theta = atan2(drone.state[1]-target[1], drone.state[0]-target[0]);
+//     }
+//     // increment the angle of the orbit by thetaStep
+//     drone.theta += thetaStep;
+//     // set the x and y of the commanded point
+//     x = target[0] + r*cos(drone.theta);
+//     y = target[1] + r*sin(drone.theta);
 
-    // if the commanded point would place the drone outside the bounds,
-    // change the commanded point to move along the boundary instead
-    if (x > (env.bounds[1][0]-1)) {
-        x = env.bounds[1][0]-1;
-    }
-    else if (x < (env.bounds[0][0]+1)) {
-        x = env.bounds[0][0]+1;
-    }
-    if (y > (env.bounds[1][1]-1)) {
-        y = env.bounds[1][1]-1;
-    }
-    else if (y < (env.bounds[0][1]+1)) {
-        y = env.bounds[0][1]+1;
-    }
+//     // if the commanded point would place the drone outside the bounds,
+//     // change the commanded point to move along the boundary instead
+//     if (x > (env.bounds[1][0]-1)) {
+//         x = env.bounds[1][0]-1;
+//     }
+//     else if (x < (env.bounds[0][0]+1)) {
+//         x = env.bounds[0][0]+1;
+//     }
+//     if (y > (env.bounds[1][1]-1)) {
+//         y = env.bounds[1][1]-1;
+//     }
+//     else if (y < (env.bounds[0][1]+1)) {
+//         y = env.bounds[0][1]+1;
+//     }
 
-    waypoint = {x, y, env.bounds[0][2]};
-    return waypoint;
-}
+//     waypoint = {x, y, env.bounds[0][2]};
+//     return waypoint;
+// }
 
 // Makes the drone fly in a square pattern around the environment bounds
 std::vector<double> MissionPlanner::bounds_trace(std::vector<double> waypoint){
