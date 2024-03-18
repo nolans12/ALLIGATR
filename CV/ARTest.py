04b277131451640e52bcdd58c4a0b6852b54a97e
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-03-18 13:24:34 -0600

    Update ARTest.py

--- CV/ARTest.py
+++ CV/ARTest.py
@@ -39,19 +39,20 @@ finalDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_type])
 
 # Try to open the 0 index for the secondary camera
 camera_index = 0   
+camera_index2 = 1
 camera_found = False
 
 while not camera_found:
     # Setup the GStreamer Pipeline
     #pipeline = f'nvarguscamerasrc sensor-id={camera_index} ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
     pipeline = 'nvarguscamerasrc sensor-id=' + str(camera_index) + ' ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
-
+    
     # Create a VideoCapture object with the GStreamer pipeline
     cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
     # Check if the camera opened successfully
     if cap.isOpened():
-        print("Camera Connected!")
+        print("Camera 0 Connected!")
         camera_found = True     # Camera is found
 
         # Get the frame width and height
@@ -60,7 +61,7 @@ while not camera_found:
         size = (frame_width, frame_height) 
         
         # Create video writer object
-        writeObj = cv2.VideoWriter('capturedVideo.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15, size) 
+        #writeObj = cv2.VideoWriter('capturedVideo.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15, size) 
         
         break
 
@@ -71,6 +72,36 @@ while not camera_found:
         faux_camera = True
         camera_found = True
 
+camera_found = 0
+while not camera_found:
+    # Setup the GStreamer Pipeline
+    #pipeline = f'nvarguscamerasrc sensor-id={camera_index} ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+    pipeline2 = 'nvarguscamerasrc sensor-id=' + str(camera_index2) + ' ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+
+    # Create a VideoCapture object with the GStreamer pipeline
+    cap2 = cv2.VideoCapture(pipeline2, cv2.CAP_GSTREAMER)
+
+    # Check if the camera opened successfully
+    if cap2.isOpened():
+        print("Camera 1 Connected!")
+        camera_found = True     # Camera is found
+
+        # Get the frame width and height
+        frame_width = int(cap.get(3)) 
+        frame_height = int(cap.get(4)) 
+        size = (frame_width, frame_height) 
+        
+        # Create video writer object
+        #writeObj = cv2.VideoWriter('capturedVideo.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15, size) 
+        
+        break
+
+    camera_index += 1
+
+    if camera_index > 10:
+        print("Camera not found after 10 attempts.")
+        faux_camera = True
+        camera_found = True
 
 # Begin the main loop that consistently outputs AR tag corners when running
 while True:
@@ -86,7 +117,7 @@ while True:
 
         # Save the frame every 4 frames (15 fps)
         if boolCapture >= 3:
-            writeObj.write(img)
+            #writeObj.write(img)
             boolCapture = 0
         else:
             boolCapture = boolCapture + 1
@@ -100,12 +131,12 @@ while True:
             break
     
     except KeyboardInterrupt:
-        writeObj.release()
+        #writeObj.release()
         cv2.destroyAllWindows()         # Close everything and release the camera
         cap.release()
         break
     
     
-writeObj.release()
+#writeObj.release()
 cv2.destroyAllWindows()         # Close everything and release the camera
 cap.release()
